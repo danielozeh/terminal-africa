4fa74aba2b71b19b52bea23a0dec44ce13d71d08
Daniel Ozeh <danielozeh@gmail.com>
2023-06-14 14:50:45 +0100

    address module for terminal africa

--- config/terminal_africa.php
+++ config/terminal_africa.php
@@ -4,5 +4,5 @@ return [
     'url' => env('TERMINAL_AFRICA_URL'),
     'secret_key' => env('TERMINAL_AFRICA_SECRET_KEY'),
     'public_key' => env('TERMINAL_AFRICA_PUBLIC_KEY'),
-    'v1' => env('TERMINAL_AFRICA_VERSION_1') || 'v1'
+    'v1' => env('TERMINAL_AFRICA_VERSION_1', 'v1')
 ];
